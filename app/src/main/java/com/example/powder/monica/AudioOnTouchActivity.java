034a6e63b36bbb0769d368db19357a05757e6243
Piotr Proszowski <215165@edu.p.lodz.pl>
2018-05-01 23:21:46 +0200

    added photo button and changed look of AudioOnTouchActivity

--- app/src/main/java/com/example/powder/monica/AudioOnTouchActivity.java
+++ app/src/main/java/com/example/powder/monica/AudioOnTouchActivity.java
@@ -57,24 +57,17 @@ public class AudioOnTouchActivity extends Activity {
 
         sendEmailButton.setOnClickListener((view)->{
 
-
             ArrayList<Uri> filesUri = new ArrayList<>();
-
-
             String path = Environment.getExternalStorageDirectory().getPath() +"/"+recorderName+"/"+ meetingName;
             File directory = new File(path);
 
             File[] files = directory.listFiles();
 
             for (File file : files) {
-
                 filesUri.add(Uri.fromFile(file));
-
             }
 
-
             File file = new File (path,"email.txt");
-
 
             Scanner in = null;
             try {
@@ -85,7 +78,6 @@ public class AudioOnTouchActivity extends Activity {
 
 
             List<String> addresses = new ArrayList<>();
-
             if(file.exists()) {
 
                 while(in.hasNext())
@@ -108,8 +100,6 @@ public class AudioOnTouchActivity extends Activity {
                 startActivity(Intent.createChooser(email, "Choose an Email client :"));
             }
             });
-
-
 
         recordButton.setOnTouchListener((v, event) -> {
             recordButton.performClick();
